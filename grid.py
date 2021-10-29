5dfe9c468f026f1763f1609871c02741dde1e8a7
zsimmons <zsimmons@umich.edu>
2021-10-29 15:25:35 -0400

    imported geopandas and obtained california geospatial data

--- grid.py
+++ grid.py
@@ -3,3 +3,16 @@
 
 # Storing all information related to each individual cell
 
+import os 
+import matplotlib.pyplot as plt
+import geopandas as gpd
+import earthpy as et
+
+# Set home directory and download data
+et.data.get_data("spatial-vector-lidar")
+os.chdir(os.path.join(et.io.HOME, 'earth-analytics'))
+
+# Import shapefile using geopandas
+sjer_plot_locations = gpd.read_file('data/spatial-vector-lidar/california/neon-sjer-site/vector_data/SJER_plot_centroids.shp')
+
+sjer_plot_locations.head(6)
